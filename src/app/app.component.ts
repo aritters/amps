9467e0942052c80167c16984dcf006f4ab7a49d5
Anderson Ritter de Souza <anderdsouza@gmail.com>
2019-09-02 11:33:24 -0300

    add angular material and basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import { MenuItem } from './shared/models/menu-item';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app-starter';
+  menuItems: MenuItem[] = [
+    { path: '/dashboard', icon: 'dashboard', title: 'Dashboard' }
+  ];
+
+  constructor() {
+  }
 }
